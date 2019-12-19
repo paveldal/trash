50c8b99686fcfcd74ac21a5c27f5144a90809128
Garibaldini85 <Garibaldini85@users.github.com>
2019-12-19 21:12:11 +0200

    Update main.cpp

--- Grebner/main.cpp
+++ Grebner/main.cpp
@@ -40,13 +40,35 @@ void output_polinom(int polinom[10][100][4])
         cout << endl;
     }
 }
+//
+//      LT(g)
+//
+void Senior_member(int polinom[100][4], int senior_mem[4])
+{
+    senior_mem[0] = polinom[1][0]; senior_mem[1] = polinom[1][1]; senior_mem[2] = polinom[1][2]; senior_mem[3] = polinom[1][3];
+
+    for(int i = 1; i < polinom[0][0]; i++)
+    {
+        if(senior_mem[1] < polinom[i][1])
+        {
+            senior_mem[0] = polinom[i][0]; senior_mem[1] = polinom[i][1]; senior_mem[2] = polinom[i][2]; senior_mem[3] = polinom[i][3];
+        }
+        else if(senior_mem[1] == polinom[i][1] && senior_mem[2] < polinom[i][2])
+        {
+            senior_mem[0] = polinom[i][0]; senior_mem[1] = polinom[i][1]; senior_mem[2] = polinom[i][2]; senior_mem[3] = polinom[i][3];
+        }
+        else if(senior_mem[1] == polinom[i][1] && senior_mem[2] == polinom[i][2] && senior_mem[3] < polinom[i][3])
+        {
+            senior_mem[0] = polinom[i][0]; senior_mem[1] = polinom[i][1]; senior_mem[2] = polinom[i][2]; senior_mem[3] = polinom[i][3];
+        }
+    }
+}
 int main()
 {
     int Arr_polinom[10][100][4] = {}, Arr_senior_member[10][4] = {};
     Arr_polinom[0][0][0] = 3; Arr_senior_member[0][0] = 3;
     input_polinom(Arr_polinom[1]);
     output_polinom(Arr_polinom);
-    cout << "He" << endl;
-
+    cout << "Hello World!" << endl;
     return 0;
 }
